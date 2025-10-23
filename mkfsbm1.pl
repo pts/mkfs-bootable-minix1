a6d28d119dedc400576e01f31ba15ee28d4f23d2
Peter Szabo <pts@fazekas.hu>
2025-10-23 15:58:54 +0200

    added reporting the location info of the debugger (db)

--- mkfsbm1.pl
+++ mkfsbm1.pl
@@ -179,10 +179,12 @@ if (defined($kernel_fn)) {
   $reserved_data = read_file_limited($kernel_fn, 0x400);
   my $final_cs;
   if ($reserved_data =~ m@^\xb8\xc0\x07 \x8e\xd8 \x31\xf6 \xb8.. \x8e\xc0  \x31\xff \xb9\x00\x01 [\xf2\xf3]\xa5 \xea@sx) {  # Minix 1.5 8086 kernel image with floppy boot sector. Example files: pc/disk.03, demo_dsk.ibm .
-    my($final, $menu_ds, $menu_pc, $menu_cs) = unpack("v4", substr($reserved_data, ((substr($reserved_data, 0x1fe, 2) eq "\x55\xaa") ? 0x1f6 : 0x1f8), 8));
+    # db is the debugger. It is not used in practice, and values are 0 in production images.
+    my($db_ds, $db_pc, $db_cs, $final, $menu_ds, $menu_pc, $menu_cs) = unpack("v7", substr($reserved_data, ((substr($reserved_data, 0x1fe, 2) eq "\x55\xaa") ? 0x1f0 : 0x1f2), 7 << 1));
     $final_cs = (($final - 1) << 5) + 0x60;
     my $menu_fofs = 0x200 + (($menu_cs - 0x60) << 4);
-    printf(STDERR "info: Minix kernel boot final=0x%x final_cs=0x%x menu_ds=0x%x menu_pc=0x%x menu_cs=0x%x menu_fofs=0x%x f=%s\n", $final, $final_cs, $menu_ds, $menu_pc, $menu_cs, $menu_fofs, $kernel_fn);
+    my $db_fofs = 0x200 + (($db_cs - 0x60) << 4);
+    printf(STDERR "info: Minix kernel boot final=0x%x final_cs=0x%x menu_ds=0x%x menu_pc=0x%x menu_cs=0x%x menu_fofs=0x%x db_ds=0x%x db_pc=0x%x db_cs=0x%x db_fofs=0x%x f=%s\n", $final, $final_cs, $menu_ds, $menu_pc, $menu_cs, $menu_fofs, $db_ds, $db_pc, $db_cs, $db_fofs, $kernel_fn);
     die("fatal: bad final: $kernel_fn\n") if $final < 2;
     die("fatal: bad menu_pc: $kernel_fn\n") if $menu_pc;
     die("fatal: bad menu_cs: $kernel_fn\n") if $menu_cs < 0x80;  # This is way too low.
